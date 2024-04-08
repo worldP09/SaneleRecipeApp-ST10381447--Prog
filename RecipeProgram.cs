626220cccb31e44488095aec71661e62d5ebc0f2
Phakamani  Stanley Nyoni <ST10381447@rcconnect.edu.za>
2024-04-08 22:19:20 +0200

    added user choice function to add number of recip to add

--- RecipeProgram.cs
+++ RecipeProgram.cs
@@ -7,7 +7,29 @@
         // welcoming message 
         Console.WriteLine("Hello!! welcome to Recipe App! \n(press A to get started and press x to exit)");
         
-        
+        char choice = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        if (choice == 'X'){
+            Console.WriteLine("Bye!!");
+            break;
+        }else if(choice !='A'){
+            Console.WriteLine("Thanks, Take first step.");
+        }
+        //give the user a choice to enter the number of recipes. 
+            Console.WriteLine("Enter the number of ingredients:");
+        int numIngredients = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"Enter ingredient {i + 1} name:");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Enter ingredient {i + 1} quantity:");
+                double quantity = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Enter ingredient {i + 1} unit:");
+                string unit = Console.ReadLine();
+
+                recipe.AddIngredient(name, quantity, unit);
+            }
       }
     }
  }
